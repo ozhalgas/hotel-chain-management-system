24dc31a9f41f3b00bb1f0b53c33f88c5b27d1480
Bakdauren Narbayev <bakdauren.narbayev@nu.edu.kz>
2020-10-23 10:53:34 +0600

    Revert "Added DAOs for guest, employee, hotel"
    This reverts commit 506581bbcc958769efb8a8807c088be0983bf043.

--- src/main/java/com/bmmzz/userDAO/UserDAO.java
+++ src/main/java/com/bmmzz/userDAO/UserDAO.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.util.Base64;
 import java.util.regex.Pattern;
 
+import com.google.gson.Gson;
+
 public class UserDAO {
 	private static Connection connection = null;
 	private static String url = "jdbc:mysql://localhost:3306/?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true";
@@ -28,7 +30,7 @@ public class UserDAO {
 		}
 	}
 	
-	protected static void executeUpdate(String query) {
+	private static void executeUpdate(String query) {
 		try {
 			connection.prepareStatement(query).executeUpdate();
 		} catch (SQLException e) {
@@ -36,7 +38,7 @@ public class UserDAO {
 		}
 	}
 	
-	protected static int executeQueryINT(String query) {
+	private static int executeQueryINT(String query) {
 		try {
 			ResultSet resultSet = connection.prepareStatement(query).executeQuery();
 			resultSet.next();
@@ -48,7 +50,7 @@ public class UserDAO {
 		return -1;
 	}
 	
-	protected static ResultSet executeQuery(String query) {
+	private static ResultSet executeQuery(String query) {
 		try {
 			ResultSet resultSet = connection.prepareStatement(query).executeQuery();
 			return resultSet;
@@ -57,6 +59,55 @@ public class UserDAO {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	public static void addEmployee(EmployeeRegistrationInfo employee) {
+		if(userExists(employee.login, "employee"))
+			return;
+
+		String employeeID = "1"; 
+		if( executeQueryINT("SELECT COUNT(*) FROM mydb.guest") > 0 ) {
+			try {
+				ResultSet resultSet = executeQuery("SELECT EmployeeID FROM mydb.employee ORDER BY EmployeeID DESC LIMIT 1;");
+				resultSet.next();
+				employeeID = Integer.toString(Integer.parseInt(resultSet.getString(1)) + 1);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}	
+		
+		String position = "";
+		if(employee.position.equals("Desk Clerk"))
+			position = "desk-clerk";
+		executeUpdate("INSERT INTO mydb.employee VALUES ('" + employeeID +"', '" + employee.fullName + "', '" + employee.gender + "', "
+				+ "'" + employee.dateOfBirth +"', '" + employee.identificationType + "', '" + employee.identificationNumber + "', "
+				+ "'" + employee.citizenship + "', '" + employee.visa + "', '" + employee.address + "', '" + employee.bankCardNumber + "', "
+				+ "'" + employee.emailAddress + "', '" + employee.homePhoneNumber + "', '" + employee.mobilePhoneNumber + "', '" + employee.login + "', "
+				+ "'" + employee.password + "')");
+		executeUpdate("INSERT INTO mydb.employee_at_hotel VALUES ('" + employeeID + "', '" + employee.hotelID + "', '" + position + "', "
+				+ "'" + employee.status + "', '" + employee.payRate + "', '" + employee.startDate + "', " + employee.getEndDate() + ")");
+	}
+	
+	public static void addGuest(GuestRegistrationInfo guest) {
+		if(userExists(guest.login, "guest"))
+					return;
+		
+		String guestID = "1"; 
+		if( executeQueryINT("SELECT COUNT(*) FROM mydb.guest") > 0 ) {
+			try {
+				ResultSet resultSet = executeQuery("SELECT guestID FROM mydb.guest ORDER BY guestID DESC LIMIT 1;");
+				resultSet.next();
+				guestID = Integer.toString(Integer.parseInt(resultSet.getString(1)) + 1);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		executeUpdate("insert into mydb.guest "
+		+ "(GuestID, FullName, IdentificationType, IdentificationNumber, Category, Address, HomePhoneNumber, MobilePhoneNumber, Login, Password) "
+		+ "values "
+		+ "('" + guestID + "', '" + guest.fullName +"', '" + guest.identificationType + "', '" + guest.identificationNumber + "', "
+		+ "'" + guest.category + "', '" + guest.address +"', '" + guest.homePhoneNumber + "', '" + guest.mobilePhoneNumber + "', '" + guest.login +"', '" + guest.password +"')");
 	}
 	
 	public static boolean userExists(String login, String table) {
@@ -73,6 +124,7 @@ public class UserDAO {
 	public static boolean userExists(String login) {
 		return userExists(login, "guest") || userExists(login, "employee");
 	}
+	
 	
 	private static boolean checkAuth(String login, String password) {
 		if(login.equals("admin") && password.equals("password") )
@@ -151,23 +203,111 @@ public class UserDAO {
 		return Base64.getEncoder().encodeToString(auth.getBytes());
 	}
 	
-	public static void addEmployee(EmployeeRegistrationInfo employee) {
-		EmployeeDAO.addEmployee(employee);
-	}
-	
-	public static void addGuest(GuestRegistrationInfo guest) {
-		GuestDAO.addGuest(guest);
-	}
-	
 	public static String getGuestInfo(String auth) {
-		return GuestDAO.getGuestInfo(auth);
+		Gson gson = new Gson();
+		GuestInfo guestInfo = new GuestInfo();
+		String json = "";
+		
+		byte[] decodedBytes = Base64.getDecoder().decode(auth);
+		String decodedAuth = new String(decodedBytes);
+		
+		if( !Pattern.compile(".+:.+").matcher(decodedAuth).matches() )
+			return null;
+		
+		String username = decodedAuth.split(":", 2)[0];
+		
+		
+		try {
+			ResultSet resultSet = executeQuery("SELECT * FROM mydb.guest WHERE Login= BINARY '" + username + "'" );
+			resultSet.next();
+			guestInfo.setGuestID( resultSet.getString(1) );
+			guestInfo.setFullName( resultSet.getString(2) );
+			guestInfo.setIdentificationType( resultSet.getString(3) );
+			guestInfo.setIdentificationNumber( resultSet.getString(4) );
+			guestInfo.setCategory( resultSet.getString(5) );
+			guestInfo.setAddress( resultSet.getString(6) );
+			guestInfo.setHomePhoneNumber( resultSet.getString(7) );
+			guestInfo.setMobilePhoneNumber( resultSet.getString(8) );
+			
+			json = gson.toJson(guestInfo, GuestInfo.class);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return json;
 	}
 	
 	public static String getEmployeeInfo(String auth) {
-		return EmployeeDAO.getEmployeeInfo(auth);
+		Gson gson = new Gson();
+		EmployeeInfo employeeInfo = new EmployeeInfo();
+		String json = "";
+		
+		byte[] decodedBytes = Base64.getDecoder().decode(auth);
+		String decodedAuth = new String(decodedBytes);
+		
+		if( !Pattern.compile(".+:.+").matcher(decodedAuth).matches() )
+			return null;
+		
+		String username = decodedAuth.split(":", 2)[0];
+		
+		try {
+			ResultSet resultSet = executeQuery("SELECT * FROM mydb.employee WHERE Login= BINARY '" + username + "'" );
+			resultSet.next();
+			employeeInfo.setEmployeeID( resultSet.getString(1) );
+			employeeInfo.setFullName( resultSet.getString(2) );
+			employeeInfo.setGender( resultSet.getString(3) );
+			employeeInfo.setDateOfBirth( resultSet.getDate(4).toString() );
+			employeeInfo.setIdentificationType( resultSet.getString(5) );
+			employeeInfo.setIdentificationNumber( resultSet.getString(6) );
+			employeeInfo.setCitizenShip( resultSet.getString(7) );
+			employeeInfo.setVise( resultSet.getString(8) );
+			employeeInfo.setAddress( resultSet.getString(9) );
+			employeeInfo.setBankCardNumber( resultSet.getString(10) );
+			employeeInfo.setEmailAdress( resultSet.getString(11) );
+			employeeInfo.setHomePhoneNumber( resultSet.getString(12) );
+			employeeInfo.setMobilePhoneNumber( resultSet.getString(13) );
+			
+			resultSet = executeQuery("SELECT * FROM mydb.employee_at_hotel WHERE Employee_EmployeeID= BINARY '" + employeeInfo.getEmployeeID() + "'" );
+			resultSet.next();
+			employeeInfo.setHotelID( resultSet.getString(2) );
+			employeeInfo.setPosition( resultSet.getString(3) );
+			employeeInfo.setStatus( resultSet.getString(4) );
+			employeeInfo.setPayrate( resultSet.getString(5) );
+			employeeInfo.setStartDate( resultSet.getDate(6).toString() );
+			if(resultSet.getDate(7) == null)
+				employeeInfo.setEndDate("NULL");
+			else
+				employeeInfo.setEndDate( resultSet.getDate(7).toString() );
+			
+			resultSet = executeQuery("SELECT Name FROM mydb.hotel WHERE HotelID= BINARY '" + employeeInfo.getHotelID() + "'" );
+			resultSet.next();
+			employeeInfo.setHotelName( resultSet.getString(1) );
+			
+			json = gson.toJson(employeeInfo, EmployeeInfo.class);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return json;
 	}
 	
 	public static String getAllHotels() {
-		return HotelDAO.getAllHotels();
+		Gson gson = new Gson();
+		String json = "";
+		Hotels hotels = new Hotels();
+		
+		try {
+			ResultSet resultSet =  executeQuery("SELECT HotelID, Name FROM mydb.hotel;" );
+			while(resultSet.next()) {
+				hotels.addHotel(resultSet.getString(1), resultSet.getString(2));
+			}
+			json = gson.toJson(hotels, Hotels.class);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return json;
 	}
 }
