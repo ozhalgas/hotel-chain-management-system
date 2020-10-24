82e251fc9aed14c4bd3d894c3732e86e388efa08
Margulan <kazikhanm21@mail.ru>
2020-10-25 04:03:02 +0600

    Basic hotels were added

---
+++ base.sql
@@ -0,0 +1,109 @@
+Insert into mydb.hotel values (0, 'Regula', 'Spain', 'Barcelona', 'Messi 10');
+Insert into mydb.hotel values (1, 'Exclusive', 'USA', 'New York', 'Wall street 23');
+Insert into mydb.hotel values (2, 'Luxury', 'UAE', 'Dubai', 'Sheikh 7');
+
+Insert into mydb.phone_number values ('100', 0);
+Insert into mydb.phone_number values ('101', 1);
+Insert into mydb.phone_number values ('102', 2);
+
+Insert into mydb.room_type values ('Single', 26.0, 1, 5, 5, 0);
+Insert into mydb.room_type values ('Double', 32.0, 2, 5, 5, 0);
+
+Insert into mydb.room_type values ('Single', 22.0, 1, 8, 8, 1);
+Insert into mydb.room_type values ('2 bed Double', 28.0, 2, 6, 6, 1);
+Insert into mydb.room_type values ('1 bed Double', 26.0, 2, 4, 4, 1);
+Insert into mydb.room_type values ('Luxury', 52.0, 4, 2, 2, 1);
+
+Insert into mydb.room_type values ('Double', 28.0, 2, 25, 25, 2);
+Insert into mydb.room_type values ('Luxury', 60.0, 4, 4, 4, 2);
+Insert into mydb.room_type values ('President', 120.0, 8, 1, 1, 2);
+
+Insert into mydb.feature values ('View to the sea', 'Double', 0);
+Insert into mydb.feature values ('Kitchen', 'Single', 1);
+Insert into mydb.feature values ('Kitchen', '2 bed double', 1);
+Insert into mydb.feature values ('View to the city', 'Luxury', 1);
+Insert into mydb.feature values ('Jacuzzi', 'Luxury', 1);
+Insert into mydb.feature values ('Balcony', 'Luxury', 1);
+Insert into mydb.feature values ('View to the sea', 'Double', 2);
+Insert into mydb.feature values ('Balcony', 'Double', 2);
+Insert into mydb.feature values ('View to the sea', 'Luxury', 2);
+Insert into mydb.feature values ('Jacuzzi', 'Luxury', 2);
+Insert into mydb.feature values ('Balcony', 'Luxury', 2);
+Insert into mydb.feature values ('View to the sea', 'President', 2);
+Insert into mydb.feature values ('View to the city', 'President', 2);
+Insert into mydb.feature values ('Terrace', 'President', 2);
+Insert into mydb.feature values ('Jacuzzi', 'President', 2);
+Insert into mydb.feature values ('Smart Security', 'President', 2);
+
+Insert into mydb.room values ('10', 1, 1, 0, 0, 'Single', 0);
+Insert into mydb.room values ('11', 1, 1, 0, 0, 'Single', 0);
+Insert into mydb.room values ('12', 1, 1, 0, 0, 'Single', 0);
+Insert into mydb.room values ('13', 1, 1, 0, 0, 'Double', 0);
+Insert into mydb.room values ('14', 1, 1, 0, 0, 'Double', 0);
+Insert into mydb.room values ('20', 2, 1, 0, 0, 'Single', 0);
+Insert into mydb.room values ('21', 2, 1, 0, 0, 'Single', 0);
+Insert into mydb.room values ('22', 2, 1, 0, 0, 'Double', 0);
+Insert into mydb.room values ('23', 2, 1, 0, 0, 'Double', 0);
+Insert into mydb.room values ('24', 2, 1, 0, 0, 'Double', 0);
+
+Insert into mydb.room values ('10', 1, 1, 0, 0, 'Single', 1);
+Insert into mydb.room values ('11', 1, 1, 0, 0, 'Single', 1);
+Insert into mydb.room values ('12', 1, 1, 0, 0, 'Single', 1);
+Insert into mydb.room values ('13', 1, 1, 0, 0, 'Single', 1);
+Insert into mydb.room values ('14', 1, 1, 0, 0, '1 bed Double', 1);
+Insert into mydb.room values ('15', 1, 1, 0, 0, '1 bed Double', 1);
+Insert into mydb.room values ('16', 1, 1, 0, 0, '2 bed Double', 1);
+Insert into mydb.room values ('17', 1, 1, 0, 0, '2 bed Double', 1);
+Insert into mydb.room values ('18', 1, 1, 0, 0, '2 bed Double', 1);
+Insert into mydb.room values ('19', 1, 1, 0, 0, '2 bed Double', 1);
+Insert into mydb.room values ('20', 2, 1, 0, 0, 'Single', 1);
+Insert into mydb.room values ('21', 2, 1, 0, 0, 'Single', 1);
+Insert into mydb.room values ('22', 2, 1, 0, 0, 'Single', 1);
+Insert into mydb.room values ('23', 2, 1, 0, 0, 'Single', 1);
+Insert into mydb.room values ('24', 2, 1, 0, 0, '1 bed Double', 1);
+Insert into mydb.room values ('25', 2, 1, 0, 0, '1 bed Double', 1);
+Insert into mydb.room values ('26', 2, 1, 0, 0, '2 bed Double', 1);
+Insert into mydb.room values ('27', 2, 1, 0, 0, '2 bed Double', 1);
+Insert into mydb.room values ('28', 2, 1, 0, 0, 'Luxury', 1);
+Insert into mydb.room values ('29', 2, 1, 0, 0, 'Luxury', 1);
+
+Insert into mydb.room values ('10', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('11', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('12', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('13', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('14', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('15', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('16', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('17', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('18', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('19', 1, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('20', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('21', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('22', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('23', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('24', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('25', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('26', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('27', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('28', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('29', 2, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('30', 3, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('31', 3, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('32', 3, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('33', 3, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('34', 3, 1, 0, 0, 'Double', 2);
+Insert into mydb.room values ('35', 3, 1, 0, 0, 'Luxury', 2);
+Insert into mydb.room values ('36', 3, 1, 0, 0, 'Luxury', 2);
+Insert into mydb.room values ('37', 3, 1, 0, 0, 'Luxury', 2);
+Insert into mydb.room values ('38', 3, 1, 0, 0, 'Luxury', 2);
+Insert into mydb.room values ('39', 3, 1, 0, 0, 'President', 2);
+
+Insert into mydb.employee values (0, 'Carles Puyol', 'Male', '1978-04-13', 'Passport', '34567', 'Spain', '76543', 'Guardiola 5', '56789', 'puyol@mail.com', '98765', '56789', 'carles', 'puyol');
+Insert into mydb.employee values (1, 'Jordan Belfort', 'Male', '1962-07-09', 'ID', '12345', 'USA', '90909', 'Di Caprio 13', '35718', 'belfort@mail.com', '177359', '973569', 'jordan', 'belfort');
+Insert into mydb.employee values (2, 'Kim Kardashian', 'Female', '1980-10-21', 'Passport', '386359', 'USA', '673526', 'West street 47A', '763527', 'kardashian@mail.com', '72567645', '2347658', 'kim', 'money');
+
+
+
+
+
+
