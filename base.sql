1fdc88b1a57f3513a310d93cd35ea2c2acf763a9
Margulan <kazikhanm21@mail.ru>
2020-10-26 04:21:11 +0600

    DB values updated
    H - Sunday as Holiday

--- base.sql
+++ base.sql
@@ -102,8 +102,134 @@ Insert into mydb.employee values (0, 'Carles Puyol', 'Male', '1978-04-13', 'Pass
 Insert into mydb.employee values (1, 'Jordan Belfort', 'Male', '1962-07-09', 'ID', '12345', 'USA', '90909', 'Di Caprio 13', '35718', 'belfort@mail.com', '177359', '973569', 'jordan', 'belfort');
 Insert into mydb.employee values (2, 'Kim Kardashian', 'Female', '1980-10-21', 'Passport', '386359', 'USA', '673526', 'West street 47A', '763527', 'kardashian@mail.com', '72567645', '2347658', 'kim', 'money');
 
+Insert into mydb.schedule values (0, 0, 'Manager', 'Full-time', '$2000', '2018-06-24', null, '09:00', '18:00');
+Insert into mydb.schedule values (1, 1, 'Manager', 'Full-time', '$3000', '2016-02-04', null, '08:30', '17:30');
+Insert into mydb.schedule values (2, 2, 'Manager', 'Full-time', '$4500', '2018-06-24', null, '09:00', '18:00');
+
+Insert into mydb.category values ('None', 0, 0);
+Insert into mydb.category values ('VIP', 0.05, 0);
+Insert into mydb.category values ('Military', 0.1, 0);
+Insert into mydb.category values ('Government', 0.1, 0);
+Insert into mydb.category values ('Veteran', 0.15, 0);
+Insert into mydb.category values ('Disabled', 0.15, 0);
+
+Insert into mydb.category values ('None', 0, 1);
+Insert into mydb.category values ('VIP', 0.1, 1);
+Insert into mydb.category values ('Military', 0.1, 1);
+Insert into mydb.category values ('Government', 0.05, 1);
+Insert into mydb.category values ('Veteran', 0.1, 1);
+Insert into mydb.category values ('Disabled', 0.15, 1);
 
 
+Insert into mydb.category values ('None', 0, 2);
+Insert into mydb.category values ('VIP', 0.15, 2);
+Insert into mydb.category values ('Military', 0.1, 2);
+Insert into mydb.category values ('Government', 0.2, 2);
+Insert into mydb.category values ('Veteran', 0.05, 2);
+Insert into mydb.category values ('Disabled', 0.1, 2);
+
+Insert into mydb.time_period values ('M', 'regular', '2010-01-01', '2030-12-31');
+Insert into mydb.time_period values ('T', 'regular', '2010-01-01', '2030-12-31');
+Insert into mydb.time_period values ('W', 'regular', '2010-01-01', '2030-12-31');
+Insert into mydb.time_period values ('R', 'regular', '2010-01-01', '2030-12-31');
+Insert into mydb.time_period values ('F', 'regular', '2010-01-01', '2030-12-31');
+Insert into mydb.time_period values ('S', 'regular', '2010-01-01', '2030-12-31');
+Insert into mydb.time_period values ('H', 'regular', '2010-01-01', '2030-12-31');
+
+Insert into mydb.operates_during values (0, 'M', 'regular');
+Insert into mydb.operates_during values (0, 'T', 'regular');
+Insert into mydb.operates_during values (0, 'W', 'regular');
+Insert into mydb.operates_during values (0, 'R', 'regular');
+Insert into mydb.operates_during values (0, 'F', 'regular');
+Insert into mydb.operates_during values (0, 'S', 'regular');
+Insert into mydb.operates_during values (0, 'H', 'regular');
 
 
+Insert into mydb.operates_during values (1, 'M', 'regular');
+Insert into mydb.operates_during values (1, 'T', 'regular');
+Insert into mydb.operates_during values (1, 'W', 'regular');
+Insert into mydb.operates_during values (1, 'R', 'regular');
+Insert into mydb.operates_during values (1, 'F', 'regular');
+Insert into mydb.operates_during values (1, 'S', 'regular');
+Insert into mydb.operates_during values (1, 'H', 'regular');
 
+
+Insert into mydb.operates_during values (2, 'M', 'regular');
+Insert into mydb.operates_during values (2, 'T', 'regular');
+Insert into mydb.operates_during values (2, 'W', 'regular');
+Insert into mydb.operates_during values (2, 'R', 'regular');
+Insert into mydb.operates_during values (2, 'F', 'regular');
+Insert into mydb.operates_during values (2, 'S', 'regular');
+Insert into mydb.operates_during values (2, 'H', 'regular');
+
+Insert into mydb.initial_price values ('Single', 0, 'M', 'regular', 60.0);
+Insert into mydb.initial_price values ('Single', 0, 'T', 'regular', 60.0);
+Insert into mydb.initial_price values ('Single', 0, 'W', 'regular', 60.0);
+Insert into mydb.initial_price values ('Single', 0, 'R', 'regular', 60.0);
+Insert into mydb.initial_price values ('Single', 0, 'F', 'regular', 65.0);
+Insert into mydb.initial_price values ('Single', 0, 'S', 'regular', 70.0);
+Insert into mydb.initial_price values ('Single', 0, 'H', 'regular', 70.0);
+
+Insert into mydb.initial_price values ('Double', 0, 'M', 'regular', 80.0);
+Insert into mydb.initial_price values ('Double', 0, 'T', 'regular', 80.0);
+Insert into mydb.initial_price values ('Double', 0, 'W', 'regular', 80.0);
+Insert into mydb.initial_price values ('Double', 0, 'R', 'regular', 80.0);
+Insert into mydb.initial_price values ('Double', 0, 'F', 'regular', 85.0);
+Insert into mydb.initial_price values ('Double', 0, 'S', 'regular', 90.0);
+Insert into mydb.initial_price values ('Double', 0, 'H', 'regular', 90.0);
+
+Insert into mydb.initial_price values ('Single', 1, 'M', 'regular', 80.0);
+Insert into mydb.initial_price values ('Single', 1, 'T', 'regular', 80.0);
+Insert into mydb.initial_price values ('Single', 1, 'W', 'regular', 80.0);
+Insert into mydb.initial_price values ('Single', 1, 'R', 'regular', 80.0);
+Insert into mydb.initial_price values ('Single', 1, 'F', 'regular', 85.0);
+Insert into mydb.initial_price values ('Single', 1, 'S', 'regular', 90.0);
+Insert into mydb.initial_price values ('Single', 1, 'H', 'regular', 90.0);
+
+Insert into mydb.initial_price values ('1 bed Double', 1, 'M', 'regular', 100.0);
+Insert into mydb.initial_price values ('1 bed Double', 1, 'T', 'regular', 100.0);
+Insert into mydb.initial_price values ('1 bed Double', 1, 'W', 'regular', 100.0);
+Insert into mydb.initial_price values ('1 bed Double', 1, 'R', 'regular', 100.0);
+Insert into mydb.initial_price values ('1 bed Double', 1, 'F', 'regular', 105.0);
+Insert into mydb.initial_price values ('1 bed Double', 1, 'S', 'regular', 110.0);
+Insert into mydb.initial_price values ('1 bed Double', 1, 'H', 'regular', 110.0);
+
+Insert into mydb.initial_price values ('2 bed Double', 1, 'M', 'regular', 110.0);
+Insert into mydb.initial_price values ('2 bed Double', 1, 'T', 'regular', 110.0);
+Insert into mydb.initial_price values ('2 bed Double', 1, 'W', 'regular', 110.0);
+Insert into mydb.initial_price values ('2 bed Double', 1, 'R', 'regular', 110.0);
+Insert into mydb.initial_price values ('2 bed Double', 1, 'F', 'regular', 115.0);
+Insert into mydb.initial_price values ('2 bed Double', 1, 'S', 'regular', 120.0);
+Insert into mydb.initial_price values ('2 bed Double', 1, 'H', 'regular', 120.0);
+
+Insert into mydb.initial_price values ('Luxury', 1, 'M', 'regular', 500.0);
+Insert into mydb.initial_price values ('Luxury', 1, 'T', 'regular', 500.0);
+Insert into mydb.initial_price values ('Luxury', 1, 'W', 'regular', 500.0);
+Insert into mydb.initial_price values ('Luxury', 1, 'R', 'regular', 500.0);
+Insert into mydb.initial_price values ('Luxury', 1, 'F', 'regular', 550.0);
+Insert into mydb.initial_price values ('Luxury', 1, 'S', 'regular', 600.0);
+Insert into mydb.initial_price values ('Luxury', 1, 'H', 'regular', 600.0);
+
+Insert into mydb.initial_price values ('Double', 2, 'M', 'regular', 150.0);
+Insert into mydb.initial_price values ('Double', 2, 'T', 'regular', 150.0);
+Insert into mydb.initial_price values ('Double', 2, 'W', 'regular', 150.0);
+Insert into mydb.initial_price values ('Double', 2, 'R', 'regular', 150.0);
+Insert into mydb.initial_price values ('Double', 2, 'F', 'regular', 175.0);
+Insert into mydb.initial_price values ('Double', 2, 'S', 'regular', 200.0);
+Insert into mydb.initial_price values ('Double', 2, 'H', 'regular', 200.0);
+
+Insert into mydb.initial_price values ('Luxury', 2, 'M', 'regular', 400.0);
+Insert into mydb.initial_price values ('Luxury', 2, 'T', 'regular', 400.0);
+Insert into mydb.initial_price values ('Luxury', 2, 'W', 'regular', 400.0);
+Insert into mydb.initial_price values ('Luxury', 2, 'R', 'regular', 400.0);
+Insert into mydb.initial_price values ('Luxury', 2, 'F', 'regular', 450.0);
+Insert into mydb.initial_price values ('Luxury', 2, 'S', 'regular', 500.0);
+Insert into mydb.initial_price values ('Luxury', 2, 'H', 'regular', 500.0);
+
+Insert into mydb.initial_price values ('President', 2, 'M', 'regular', 3000.0);
+Insert into mydb.initial_price values ('President', 2, 'T', 'regular', 3000.0);
+Insert into mydb.initial_price values ('President', 2, 'W', 'regular', 3000.0);
+Insert into mydb.initial_price values ('President', 2, 'R', 'regular', 3000.0);
+Insert into mydb.initial_price values ('President', 2, 'F', 'regular', 3500.0);
+Insert into mydb.initial_price values ('President', 2, 'S', 'regular', 4000.0);
+Insert into mydb.initial_price values ('President', 2, 'H', 'regular', 4000.0);
