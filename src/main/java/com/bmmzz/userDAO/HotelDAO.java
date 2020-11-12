9bdcfa32df3d3d6a0e9a3a0704c0756e43a40b54
ozhalgas <56766998+ozhalgas@users.noreply.github.com>
2020-11-13 02:54:14 +0600

    guestID, CID, COD are available in rooms json + bug fixes

--- src/main/java/com/bmmzz/userDAO/HotelDAO.java
+++ src/main/java/com/bmmzz/userDAO/HotelDAO.java
@@ -2,6 +2,11 @@ package com.bmmzz.userDAO;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import javax.sql.RowSet;
 
@@ -102,6 +107,27 @@ public class HotelDAO {
 		return json;
 	}
 	
+	public static void setRoomOccupied(int roomNumber, int occupied) {
+		HotelRoomsInfo hotelRooms = new HotelRoomsInfo();
+		hotelRooms.setOccupied(roomNumber, occupied);
+	}
+	
+	public static void setGuestInRoom(int roomNumber, int guestID) {
+		HotelRoomsInfo hotelRooms = new HotelRoomsInfo();
+		hotelRooms.setGuestInRoom(roomNumber, guestID);
+	}
+	
+	public static void setCID(int roomNumber, String CID) {
+		HotelRoomsInfo hotelRooms = new HotelRoomsInfo();
+		hotelRooms.setCID(roomNumber, CID);
+	}
+	
+	public static void setCOD(int roomNumber, String COD) {
+		HotelRoomsInfo hotelRooms = new HotelRoomsInfo();
+		hotelRooms.setCOD(roomNumber, COD);
+	}
+	
+	//hotelID and roomTypeNames should be considered
 	public static String getHotelRooms(String auth) {
 		Gson gson = new Gson();
 		HotelRoomsInfo hotelRooms = new HotelRoomsInfo();
@@ -109,10 +135,70 @@ public class HotelDAO {
 		
 		String username = UserDAO.getDecodedAuth(auth)[0];
 		
+		//SingleStays for today
+		List<String> checkInD = new ArrayList<>();
+		List<String> checkOutD = new ArrayList<>();
+		List<String> roomN = new ArrayList<>();
+		List<Integer> roomF = new ArrayList<>();
+		List<Integer> gID = new ArrayList<>();
+				
+		//guestIDs that will be full-filled into hotelRooms
+		List<Integer> roomGuestID = new ArrayList<>();
+		List<String> roomCID = new ArrayList<>();
+		List<String> roomCOD = new ArrayList<>();
+		
+		String todayStr = java.time.LocalDate.now().toString();
+		Date todayDate = new Date();
 		try {
-			ResultSet resultSet = UserDAO.executeQuery("SELECT * FROM mydb.employee, mydb.schedule, mydb.room, mydb.hotel WHERE mydb.Employee.Login= BINARY 'gal' and mydb.employee.EmployeeID = mydb.schedule.EmployeeID and mydb.schedule.HotelID = mydb.room.HotelID and mydb.schedule.HotelID = mydb.hotel.HotelID" );
+			todayDate = new SimpleDateFormat("yyyy-mm-dd").parse(todayStr);
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			ResultSet resSS = UserDAO.executeQuery("SELECT * FROM mydb.single_stay");
+			while(resSS.next()) {
+				Date tempCheckOutDate = new Date();
+				try {
+					tempCheckOutDate = new SimpleDateFormat("yyyy-mm-dd").parse(resSS.getString(2));
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if(todayDate.before(tempCheckOutDate)) {
+					checkInD.add(resSS.getString(1));
+					checkOutD.add(resSS.getString(2));
+					roomN.add(resSS.getString(4));
+					roomF.add(resSS.getInt(5));
+					gID.add(resSS.getInt(6));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {	
+			ResultSet resultSet = UserDAO.executeQuery("SELECT * FROM mydb.employee, mydb.schedule, mydb.room, mydb.hotel WHERE mydb.Employee.Login= BINARY '" + username+ "' and mydb.employee.EmployeeID = mydb.schedule.EmployeeID and mydb.schedule.HotelID = mydb.room.HotelID and mydb.schedule.HotelID = mydb.hotel.HotelID" );
+			int ind = 0;
 			while(resultSet.next()) {
-				hotelRooms.addRoom( resultSet.getString(25), resultSet.getInt(26), resultSet.getInt(27), resultSet.getInt(28), resultSet.getInt(29), resultSet.getString(30), resultSet.getInt(31), resultSet.getNString(33));
+				boolean exists = false;
+				for(int i = 0; i < roomN.size(); ++i) {
+					if(resultSet.getString(25).equals(roomN.get(i)) && resultSet.getInt(26) == roomF.get(i)) {
+						roomGuestID.add(gID.get(i));
+						roomCID.add(checkInD.get(i));
+						roomCOD.add(checkOutD.get(i));
+						exists = true;
+						break;
+					}
+				}
+				if(!exists) { 
+					roomGuestID.add(-1);
+					roomCID.add(null);
+					roomCOD.add(null);
+				}
+				hotelRooms.addRoom( resultSet.getString(25), resultSet.getInt(26), resultSet.getInt(27), resultSet.getInt(28), resultSet.getInt(29), resultSet.getString(30), resultSet.getInt(31), resultSet.getNString(33), roomGuestID.get(ind), roomCID.get(ind), roomCOD.get(ind));
+				++ind;
 			}
 			json = gson.toJson(hotelRooms, HotelRoomsInfo.class);
 		} catch (SQLException e) {
