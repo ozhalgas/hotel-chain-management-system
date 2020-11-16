c0523d0f78698e53641bcb20a7bdf885b6efbee6
ozhalgas <56766998+ozhalgas@users.noreply.github.com>
2020-11-16 22:01:21 +0600

    ignore

--- src/main/java/com/bmmzz/service/SeasonService.java
+++ src/main/java/com/bmmzz/service/SeasonService.java
@@ -2,11 +2,16 @@ package com.bmmzz.service;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+
+import com.bmmzz.userDAO.EmployeeDAO;
 import com.bmmzz.userDAO.HotelDAO;
 import com.bmmzz.userDAO.SeasonDAO;
 import com.bmmzz.userDAO.UserDAO;
@@ -28,5 +33,22 @@ public class SeasonService {
 		}
 		String json = SeasonDAO.getAllSeasons(auth);
 		return Response.ok(json).build();
+	}
+	
+	@POST
+	@Path("/create/{seasonName}-{startDate}-{endDate}-{roomTypes}-{prices}")
+	public Response createTimePeriod(@DefaultValue("") @QueryParam("auth") String auth,
+								     @PathParam("seasonName") String seasonName,
+								     @PathParam("startDate") String startDate,
+								     @PathParam("endDate") String endDate,
+								     @PathParam("roomTypes") String roomTypes,
+								     @PathParam("prices") String prices,
+								     @FormParam("hotelID") int hotelID) {
+		if (!UserDAO.checkRoleAndAuth(auth, "manager"))
+			return null;
+		//SeasonDAO.createTimePeriod();
+		//SeasonDAO.createInitialPrice();
+		//SeasonDAO.createOperatesDuring();
+		return Response.ok().build();
 	}
 }
