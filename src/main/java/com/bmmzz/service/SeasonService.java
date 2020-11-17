51d9c67912330184e09eca4d07d3da63a078a1f8
Bakdauren Narbayev <58412328+bakdaurenNarbayev@users.noreply.github.com>
2020-11-17 17:59:52 +0600

    seasonal page implemented

--- src/main/java/com/bmmzz/service/SeasonService.java
+++ src/main/java/com/bmmzz/service/SeasonService.java
@@ -1,5 +1,7 @@
 package com.bmmzz.service;
 
+import java.io.InputStream;
+
 import javax.servlet.ServletContext;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
@@ -8,8 +10,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import com.bmmzz.userDAO.EmployeeDAO;
 import com.bmmzz.userDAO.HotelDAO;
@@ -24,6 +28,19 @@ public class SeasonService {
 	public SeasonService() {
 		UserDAO.connectToUserDAO();
 	}
+	
+	@GET
+    @Produces({MediaType.TEXT_HTML})
+    public InputStream get( @DefaultValue("") @QueryParam("auth") String auth ) {
+        if(!UserDAO.checkAuth(auth))
+            return Helper.getPage(servletContext, "accessDeniedPage.html");
+        switch( UserDAO.getRole(auth) ) {
+            case "manager":
+                return Helper.getPage(servletContext, "seasonPage.html");
+            default:
+                return Helper.getPage(servletContext, "accessDeniedPage.html");
+        }
+    }
 	
 	@GET
 	@Path("/all")
